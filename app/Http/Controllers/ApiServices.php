d8445fce900964a709dbaf1cba57b39adaeb2169
Oludowole Olumide <xeonncodes@gmail.com>
2024-07-01 22:38:13 +0100

    Added files

--- app/Http/Controllers/ApiServices.php
+++ app/Http/Controllers/ApiServices.php
@@ -9,14 +9,25 @@ class ApiServices extends Controller
 {
 
     // Resolve Bank (1)
-    public function ResolveBankHaven($code, $number)
+    public function ResolveBankHaven($code, $number, $token)
     {
 
         $post = Http::withHeaders([
-
-        ])->post("", [
+            "ClientID" => env("HAVEN_CLIENT_ID"),
+            "content-type" => "application/json",
+            "accept" => "application/json",
+            "Authorization" => "Bearer " . $token
+        ])->post(env("HAVEN_BASE_URL") . "/transfers/name-enquiry", [
 
                 ]);
+
+        $postJson = $post->json();
+
+        if ($postJson['statusCode'] == 200) {
+
+        } else {
+
+        }
 
     }
 
